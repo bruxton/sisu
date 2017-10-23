1d01584d73b4b5c01363e950b18156bdf065c36e
Jacob Arriola <jacob.arriola@gmail.com>
2017-10-22 19:44:05 -0700

    Add SEO tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,15 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'SISU',
+    title: 'SISU Tech Partners | A premier tech recruiting firm. Building teams that become the forefront of innovation',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Connecting devoted recruits with leading companies to advance the technology industry.'
+      },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico?v2' },
